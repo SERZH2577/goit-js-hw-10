160940223b4c841d952e74f2193c3169512e2271
Serhii Plis <serzh2577@gmail.com>
2022-02-08 00:53:10 +0200

    adds lodash.debouns, notiflix, handlebars

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBoxEl = document.querySelector('#search-box');
+
+searchBoxEl.addEventListener('input', debounce(onTextInput, `${DEBOUNCE_DELAY}`));
+
+function onTextInput(e) {
+  console.log(300);
+  // Notiflix.Notify.failure('Oops, there is no country with that name');
+
+  console.log(e.target.value.length);
+
+  if (e.target.value.length <= 1) {
+    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+  }
+}
