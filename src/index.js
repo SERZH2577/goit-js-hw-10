352a9b3bf807979bcea4cce4a3f891ee9c553f4a
Serhii Plis <serzh2577@gmail.com>
2022-02-20 09:52:36 +0200

    dop fix hw-10

--- src/index.js
+++ src/index.js
@@ -16,14 +16,17 @@ const renderCountryInfo = template => {
 };
 
 function onTextInput(e) {
-  const BASE_URL = 'https://restcountries.com';
-
   fetchCountries(e.target.value)
     .then(countries => {
       const templateCountryInfo = createCountryList(countries);
       renderCountryInfo(templateCountryInfo);
     })
-    .catch(error => console.log(error));
+    .catch(error => {
+      const incorrectInput = '';
+      renderCountryInfo(incorrectInput);
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      console.log(error);
+    });
 
   function createCountryList(countries) {
     if (countries.length === 1) {
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,11 +4,7 @@ export const fetchCountries = name => {
   return fetch(`${BASE_URL}/v3.1/name/${name}?fields=name,capital,flags,population,languages`)
     .then(res => {
       if (!res.ok) {
-        const incorrectInput = '';
-        renderCountryInfo(incorrectInput);
-        Notiflix.Notify.failure('Oops, there is no country with that name');
-
-        throw res();
+        throw new res();
       }
       return res.json();
     })
